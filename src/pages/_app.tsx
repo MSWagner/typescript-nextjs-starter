125e45b462cac7ec4b0b9a43c5e01575f249386c
Michael Wagner <michael.wagner@allaboutapps.at>
2021-07-14 08:19:15 +0200

    chore: add support for react components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/global.css";
 import { AppProps } from "next/app";
-import "@/styles/global.css";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "@/components/theme";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector(`#jss-server-side`);
+        if (jssStyles) {
+            jssStyles.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Makerspace</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant,
+                consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
+
+App.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    pageProps: PropTypes.object.isRequired,
+};
+
+export default App;
